ea6437e33bef561d60ea685888afe4fd44e8ff2a
python_tanuki <araki130710@gmail.com>
2023-05-05 23:39:52 +0900

    add regex

--- MatchingApp/LoginView/LoginViewModel.swift
+++ MatchingApp/LoginView/LoginViewModel.swift
@@ -17,14 +17,23 @@ class LoginViewModel: ObservableObject {
     var isEnabledTappedLoginButton: Bool { !isEmailEmpty && !isPasswordEmpty }
     
     func signInWithEmail(completion:@escaping ()-> Void){
-       
+        
     }
     
-    func validateEmail(){
-        isEmailEmpty = emailText == ""
+    func validateEmail() -> Bool {
+        let emailRegEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
+        let emailPred = NSPredicate(format:"SELF MATCHES %@", emailRegEx)
+        return emailPred.evaluate(with: emailText)
+//        isEmailEmpty = emailText == ""
     }
-    func validatePassword(){
-        isPasswordEmpty = passwordText == ""
+    
+    // Password should be longer than 8 and shorter than 25 characters.
+    // Also, it should be included at least one capital, number.
+    func validatePassword() -> Bool {
+        let passwardRegEx = "^(?=.*[A-Z])(?=.*[0-9])[a-zA-Z0-9]{8,24}$"
+        let passwardPred = NSPredicate(format: "SELF MATCHES %@", passwardRegEx)
+        return passwardPred.evaluate(with: passwordText)
+//        isPasswordEmpty = passwordText == ""
     }
 }
 
